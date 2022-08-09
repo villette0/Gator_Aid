f47387bb3da6f51ce78bf50aa7bbd95f8d71a8ac
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-08-08 19:56:01 -0700

    built out Models

---
+++ models/Comments.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'post',
+          key: 'id',
+        }
+    }
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments',
+  }
+);
+
+module.exports = Comments;
---
+++ models/index.js
@@ -0,0 +1,28 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comments = require('./Comments');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Comments };
