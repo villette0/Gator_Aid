474c64a088da3b473ea74340e4108dda6648e830
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-08-14 21:45:31 -0700

    signup separated from login in views and js with nav between pages, create post button added to main navigation

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -112,6 +112,16 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
 
+  router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('signup');
+  });
+
   router.get('/create-post', withAuth, async (req, res) => {
     res.render('create-post', {
       logged_in: req.session.logged_in
--- public/js/login.js
+++ public/js/login.js
@@ -22,28 +22,28 @@ const userLoginForm = async (event) => {
     }
   };
 
-  const userSignUpForm = async (event) => {
-    event.preventDefault();
+  // const userSignUpForm = async (event) => {
+  //   event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const userName = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  //   const name = document.querySelector('#name-signup').value.trim();
+  //   const userName = document.querySelector('#username-signup').value.trim();
+  //   const email = document.querySelector('#email-signup').value.trim();
+  //   const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && userName && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, userName, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+  //   if (name && userName && email && password) {
+  //     const response = await fetch('/api/users', {
+  //       method: 'POST',
+  //       body: JSON.stringify({ name, userName, email, password }),
+  //       headers: { 'Content-Type': 'application/json' },
+  //     });
   
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+  //     if (response.ok) {
+  //       document.location.replace('/dashboard');
+  //     } else {
+  //       alert(response.statusText);
+  //     }
+  //   }
+  // };
 
   document
   .querySelector('.login-form')
